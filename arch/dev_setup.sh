94f5772b62b240c9b3ea2383900d4129df36ba50
ben <ben>
2024-06-20 16:34:34 -0700

    ad

--- arch/dev_setup.sh
+++ arch/dev_setup.sh
@@ -26,6 +26,25 @@ sudo pacman -S --noconfirm $PACKAGES
 
 # <FIREFOX>
 sudo pacman -S --noconfirm firefox
+profile_folder=$(find ~/.mozilla/firefox -maxdepth 1 -type d -name "*default-release")
+if [ -z "$profile_folder" ]; then
+    echo "Firefox profile folder with 'default-release' not found."
+    exit 1
+fi
+# Path to prefs.js file in the profile folder
+prefs_js_path="$profile_folder/prefs.js"
+
+# Enable toolkit.legacyUserProfileCustomizations.stylesheets
+if ! grep -q 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' "$prefs_js_path"; then
+    echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> "$prefs_js_path"
+    echo "Enabled toolkit.legacyUserProfileCustomizations.stylesheets in prefs.js"
+else
+    echo "toolkit.legacyUserProfileCustomizations.stylesheets is already enabled"
+fi
+
+css_source=$CONFIG_PATH/firefox/chrome/userChrome.css
+mkdir -p "$profile_folder/chrome"
+cp "$css_source" "$profile_folder/chrome/userChrome.css"
 # </FIREFOX>
 
 
